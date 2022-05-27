a39ad94897eedf027135d6ca6aeb7f3e1587a636
hzlpntl <hazelrosemarie.pontilla@evsu.edu.ph>
2022-05-27 16:41:01 +0800

    change

--- index.php
+++ index.php
@@ -19,8 +19,8 @@
         <p>Operator: <br>
             <select name="operator">
                 <option value="">-</option>
-                <option value="add">Add</option>
-                <option value="subtract">Subtract</option>
+                <option value="add">Addition</option>
+                <option value="subtract">Subtraction</option>
                 <option value="multiply">Multiply</option>
                 <option value="divide">Divide</option>
             </select>
