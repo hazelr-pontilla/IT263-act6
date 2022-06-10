74f274a305a570523a84e355a63af32ae1ce1aeb
hzlpntl <hazelrosemarie.pontilla@evsu.edu.ph>
2022-06-10 11:05:34 +0800

    add styles

--- functions.php
+++ functions.php
@@ -1,19 +1,19 @@
 <?php
 
-function addNumber ($num1, $num2){
-    return $num1 + $num2;
-}
+    function addNumber ($num1, $num2){
+        return $num1 + $num2;
+    }
 
-function subtractNumber ($num1, $num2){
-    return $num1 - $num2;
-}
+    function subtractNumber ($num1, $num2){
+        return $num1 - $num2;
+    }
 
-function multiplyNumber ($num1, $num2){
-    return $num1 * $num2;
-}
+    function multiplyNumber ($num1, $num2){
+        return $num1 * $num2;
+    }
 
-function divideNumber ($num1, $num2){
-    return $num1 / $num2;
-}
+    function divideNumber ($num1, $num2){
+        return $num1 / $num2;
+    }
 
 ?>
